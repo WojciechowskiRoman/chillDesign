0b30dfa45f5a182ee69dd2811de1e484b541aca0
WojciechowskiRoman <roman.vaitsakhouski@gmail.com>
2021-06-24 21:45:54 +0200

    added filter in portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function() {
     )
     wow.init();
 
-    var mixer = mixitup('.portfolio__list');
+    // var mixer = mixitup('.portfolio__list');
 
     $('.filter__button').on('mixitup-control-active', function() {
         $.scrollify.update();
@@ -32,38 +32,72 @@ $(function() {
         overflowScroll: true,
         updateHash: false,
         touchScroll:true,
-        before:function(index) {
-           
-          },
+        before:function(index) {},
         after:function() {},
-        afterResize:function() {
-            
-        },
-        afterRender:function() {
-            
-          }
+        afterResize:function() {},
+        afterRender:function() {}
       });
 
-    // $('.portfolio__more-link').on('click', function() {
-    //     $('.more').removeClass('sr-only');
-    //     $('.portfolio__more').css('display', 'none');
-    //     // $.scrollify.update();
-        
-    // })
-
-    var index = 7;
+    let index = 7;
     $('.portfolio__box:gt('+index+')').hide();
-    $('.portfolio__show--more').on('click', function() {
+    $('.show--more').on('click', function() {
         $('.portfolio__box:gt('+index+')').fadeIn().show();
         $('.portfolio__title').text('Show less');
-        $('.portfolio__show--more').css('display', 'none');
-        $('.portfolio__show--less').css('display', 'block');
+        $('.show--more').css('display', 'none');
+        $('.show--less').css('display', 'block');
     });
 
-    $('.portfolio__show--less').on('click', function() {
+    $('.show--less').on('click', function() {
         $('.portfolio__box:gt('+index+')').hide("slow");
         $('.portfolio__title').text('WANT TO SEE MORE?');
-        $('.portfolio__show--more').css('display', 'block');
-        $('.portfolio__show--less').css('display', 'none');
+        $('.show--more').css('display', 'block');
+        $('.show--less').css('display', 'none');
     });
+
+    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+    if(menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener('click', onMenuLinkClick);
+        });
+
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__menu').offsetHeight;
+
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: 'smooth'
+                });
+                e.preventDefault();
+            }
+        }
+    }
+    
+    let list = document.querySelectorAll('.portfolio__button');
+    let item = document.querySelectorAll('.portfolio__box');
+
+    for(let i = 0; i < list.length; i++) {
+        list[i].addEventListener('click', function() {
+            for(let j = 0; j < list.length; j++) {
+                list[j].classList.remove('portfolio__button--active')
+            }
+            this.classList.add('portfolio__button--active');
+
+            let dataFilter = this.getAttribute('data-filter');
+
+            for(let k = 0; k < item.length; k++) {
+                item[k].classList.remove('active');
+                item[k].classList.add('hide');
+
+                if(item[k].getAttribute('data-item') == dataFilter ||
+                dataFilter == 'all') {
+                    item[k].classList.remove('hide');
+                    item[k].classList.add('active');
+                }
+            }
+        })
+    }
 });
